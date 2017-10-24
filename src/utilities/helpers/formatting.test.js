7f97c48f8fa3f634497d13a3768b9e808cad6198
Adhy <adhywiranatap@gmail.com>
2017-10-24 13:37:30 +0700

    add RN image picker dependency

--- src/utilities/helpers/formatting.test.js
+++ src/utilities/helpers/formatting.test.js
@@ -2,37 +2,37 @@ import formatting from './formatting';
 
 const formatDate = [{
   calledWith: null,
-  returns: '31-12-1969'
-},{
+  returns: '31-12-1969',
+}, {
   calledWith: false,
-  returns: '31-12-1969'
-},{
+  returns: '31-12-1969',
+}, {
   calledWith: true,
-  returns: '31-12-1969'
-},{
+  returns: '31-12-1969',
+}, {
   calledWith: '',
-  returns: 'aN-aN-NaN'
-},{
+  returns: 'aN-aN-NaN',
+}, {
   calledWith: '1/1/2000',
-  returns: '01-01-2000'
-},{
+  returns: '01-01-2000',
+}, {
   calledWith: '01/1/2000',
-  returns: '01-01-2000'
-},{
+  returns: '01-01-2000',
+}, {
   calledWith: '01/01/2000',
-  returns: '01-01-2000'
-},{
+  returns: '01-01-2000',
+}, {
   calledWith: '12/1/2000',
-  returns: '01-12-2000'
-},{
+  returns: '01-12-2000',
+}, {
   calledWith: '1/31/2000',
-  returns: '31-01-2000'
-},{
+  returns: '31-01-2000',
+}, {
   calledWith: '12/31/2000',
-  returns: '31-12-2000'
-},];
+  returns: '31-12-2000',
+}];
 
-formatDate.forEach(testCase => {
+formatDate.forEach((testCase) => {
   test(`formateDate, called with ${testCase.calledWith}, returns ${testCase.returns}`, () => {
     expect(formatting.formatDate(testCase.calledWith)).toBe(testCase.returns);
   });
