e266b8ad9e2dda3f42798f14e12ab6d4cbfb86e0
Adhy <adhywiranatap@gmail.com>
2017-10-24 12:36:08 +0700

    add color constants

---
+++ src/constants.js
@@ -0,0 +1,49 @@
+// colors based on http://www.flatuicolorpicker.com/
+export const COLORS = {
+  red: {
+    oldBrick: '#96281B',
+    softRed: '#EC644B',
+  },
+  purple: {
+    honeyFlower: '#674172',
+    lavender: '#947CB0',
+    plum: '#913D88',
+    wisteria: '#9B59B6',
+  },
+  blue: {
+    sanMarino: '#446CB3',
+    royal: '#4183D7',
+    picton: '#59ABE3',
+    curious: '#3498DB',
+    jellyBean: '#2574A9',
+  },
+  green: {
+    eucalyptus: '#26A65B',
+    silverTree: '#68C3A3',
+    mountainMeadow: '#1BBC9B',
+  },
+  yellow: {
+    lemon: '#F7CA18',
+  },
+  orange: {
+    mango: '#FABE58',
+    fireBush: '#EB9532',
+    tahitiGold: '#E87E04',
+    burnt: '#D35400',
+  },
+  grey: {
+    lynch: '#6C7A89',
+    silver: '#BFBFBF',
+    iron: '#DADFE1',
+    gallery: '#EEEEEE',
+  },
+  white: '#FFFFFF',
+  black: '#000000',
+};
+
+export const FONT_SIZES = {
+  small: 12,
+  medium: 14,
+  large: 16,
+};
+
