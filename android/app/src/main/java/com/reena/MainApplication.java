82b23c468faa3a706102bc31133f21102d7e6fbf
Adhy <adhywiranatap@gmail.com>
2017-11-04 11:13:26 +0700

    update native modules

--- android/app/src/main/java/com/reena/MainApplication.java
+++ android/app/src/main/java/com/reena/MainApplication.java
@@ -3,6 +3,10 @@ package com.reena;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new MapsPackage(),
+            new ImagePickerPackage(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage()
       );
     }
 
